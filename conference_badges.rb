78b626bb125f83e5af067f7021285fd2ca9d4e75
Aliciea D Suechin <adr330@nyu.edu>
2019-06-14 16:33:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,5 @@ end
 def assign_rooms(attendees)
   array = []
   attendees.each_with_index do |name,index|
-    array << "Hello,#{name}! You'll be assigned to room #{index+1}"
+    array << "Hello,#{name}! You'll be assigned to room #{index+1}!"
+  end 
